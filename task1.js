46a3fad4bcddd0c7b36c86e40957f66bec4c7e3e
husnua <husnuarkca00@gmail.com>
2019-07-03 11:48:50 +0300

    eslint eklendi, duzenlemeler yapildi.±  Please enter the commit message for your changes. Lines starting

--- task1.js
+++ task1.js
@@ -1,27 +1,37 @@
-const express  =  require('express') ;
-const port     =  3000 ;
-const app      =  express();
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const express = require('express');
+
+const port = 3000;
+const app = express();
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/',function(req,res) {
-        res.send('<!DOCTYPE html><center>'+
-        '<form action = "http://localhost:3000/kaydet" method = "POST">' +
-        '<input type="text" id=baslik name=baslik placeholder="BASLIK GIR"><br>'+
-        '<input type="text" id=metin name=metin placeholder="METIN GIR"><br><br>'+
-        '<button type=submit>GIR</button>'+
-        '</form></center>');
+let baslikVeri;
+let metinVeri;
 
-
+app.get('/', (req, res) => {
+  res.send(`<!DOCTYPE html><center>'
+    <form action = "http://localhost:${port}/kaydet" method="POST"> 
+    <input type="text" id=baslik name=baslik placeholder="BASLIK GIR"><br>
+    <input type="text" id=metin name=metin placeholder="METIN GIR"><br><br>
+    <button type=submit>GIR</button>
+    </form></center>`);
 });
 
-app.post('/kaydet',urlencodedParser, function (req,res){
-        let baslik=req.body.baslik ;
-        let metin=req.body.metin ;
-        res.send('<h1>'+baslik+'</h1><p>'+metin+'</p>');
+app.post('/kaydet', (req, res) => {
+  const { baslik = 'varsayilan', metin } = req.body;
+
+  baslikVeri = baslik;
+  metinVeri = metin;
+
+  res.send(`
+    <h1>${baslikVeri}</h1>
+    <p>${metinVeri}</p>`);
 });
 
+app.get('/icerik', (req, res) => {
+  res.send(metinVeri);
+});
 
-app.listen(port) ;
+app.listen(port);
